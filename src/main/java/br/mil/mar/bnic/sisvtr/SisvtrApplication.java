07b563be4ffcaa00d648bcfaadf0dafaf95402ce
romulospl <romulopereiralopes@gmail.com>
2023-09-18 15:31:58 -0300

    commit inicial

---
+++ src/main/java/br/mil/mar/bnic/sisvtr/SisvtrApplication.java
@@ -0,0 +1,13 @@
+package br.mil.mar.bnic.sisvtr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisvtrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisvtrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/mil/mar/bnic/sisvtr/SisvtrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mil.mar.bnic.sisvtr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisvtrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
